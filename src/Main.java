af8d1fcb73d4caa1046c9c35907d265b31b5d293
Yu Zozulya <YuZozulya@gmail.com>
2014-10-24 11:09:49 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+// some chage here
+// test
+// test
+// test
+// test
+// test
+// test
+//test
+// test
+// test
+// kkk
+// another
+// another
